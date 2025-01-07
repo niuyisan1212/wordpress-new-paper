9269a1e31b1ab8b5277036a3ddb204bfbd4ed8d2
niuyisan1212 <1097960702@qq.com>
2025-01-07 21:53:31 +0800

    新增”关于我“页面模板

---
+++ aboutme.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: 关于我
+*/
+?>
+<?php get_header(); ?>
+<h1 class="title"><?php the_title();?></h1>
+<article>
+<div class="content"><?php the_content(); ?></div>
+</article>
+<?php get_footer(); ?>
